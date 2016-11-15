d2d0054e7d8c59a05fd426bf4db0c0e3f94462fb
drum2007 <beam555@hotmail.com>
2016-11-15 14:47:43 +0700

    User done

--- src/User.java
+++ src/User.java
@@ -1,26 +1,55 @@
 public class User implements IUser {
+    private String username;
+    private String password;
+    private static final String NAME_CHECK = "[A-Z][a-z]{2,} [A-Z][a-z]{2,}";
+    private static final String PASSWORD_CHECK = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{8,}";
+    private int type;
+
+    public User() {
+        username = DEFUALT_NAME;
+        password = null;
+    }
+
+
     @Override
     public String getName() {
-        return null;
+        return username;
     }
 
     @Override
     public String setName(String name) {
-        return null;
+        if (isNameCorrect(name))
+            username = name;
+        return username;
+    }
+
+    @Override
+    public boolean isNameCorrect(String name) {
+        if (name == null) return false;
+        return name.matches(NAME_CHECK);
     }
 
     @Override
     public String setPassword(String password) {
-        return null;
+        if (isPasswordCorrect(password))
+            this.password = password;
+        return this.password;
     }
 
     @Override
     public boolean isPasswordCorrect(String password) {
-        return false;
+        if (password == null) return false;
+        return password.matches(PASSWORD_CHECK);
     }
 
     @Override
     public int getType() {
-        return 0;
+        return type;
+    }
+
+    @Override
+    public void setType(int type) {
+        if (type == USER_STUDENT || type == USER_TEACHER)
+        this.type = type;
     }
 }
